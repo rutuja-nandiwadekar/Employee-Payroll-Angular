308b43813c780341d61511176b86e994dbbfaa0e
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2023-03-21 11:28:24 +0530

    Added: Header and routing for home and add-employee pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
-
+  { path: '', redirectTo: 'home-page', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'add-user', component: AddEmployeeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddEmployeeComponent,
+    HomePageComponent,
     HeaderComponent
   ],
   imports: [
